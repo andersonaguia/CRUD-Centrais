39a86111a212e73ebc55080513595283642ef277
Anderson Aguiar <andersonlaguiar@gmail.com>
2025-08-27 11:09:33 -0300

    feat: cria service para integração com banco de dados

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
+import { DatabaseModule } from '../database/database.module';
 
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
