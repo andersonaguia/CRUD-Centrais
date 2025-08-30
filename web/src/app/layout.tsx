d74d0f6cf7d3a363a499e94d722f9b32c7aa52c8
Anderson Aguiar <andersonlaguiar@gmail.com>
2025-08-30 08:38:32 -0300

    feat: cria provider para disponibilização do react query

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,15 +3,21 @@ import { ReactNode } from "react";
 
 import { fonts } from "@components/styles/fonts";
 import "@styles/global.css";
+import { ReactQueryProvider } from "@components/providers/query-client-provider";
 
 export const metadata: Metadata = {
   title: "Defense IA | Middlewares e Centrais",
 };
 
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br" className={fonts.nunito}>
-      <body>{children}</body>
+      <body>
+        <ReactQueryProvider>
+          {children}
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
