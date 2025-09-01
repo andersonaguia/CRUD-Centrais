3be5c728d1c6cdda0ce7b703c0d0dba1314d6e09
Anderson Aguiar <andersonlaguiar@gmail.com>
2025-08-31 23:56:44 -0300

    feat: atualiza total de centrais no header quando há exclusão de central

--- web/src/presentation/components/notifications/notifications.tsx
+++ web/src/presentation/components/notifications/notifications.tsx
@@ -4,15 +4,23 @@ import { useEffect } from "react";
 import { io, Socket } from "socket.io-client";
 import { API_URL } from "../../../common/constants";
 import { toast } from "react-toastify";
+import { useCentralStore } from "../../../stores/central.store";
+import { CentralNotification } from "./types";
 
 let socket: Socket;
 
 export default function Notifications() {
+  const { setTotalCentrals } = useCentralStore();
   useEffect(() => {
     socket = io(`${API_URL}/notifications`);
 
-    socket.on("newCentral", (message) => {
-      toast.info(message);
+    socket.on("newCentral", (notification: CentralNotification) => {
+      toast.info(notification.message);
+      setTotalCentrals(notification.totalCentrals);
+    });
+
+    socket.on("removedCentral", (notification: CentralNotification) => {
+      setTotalCentrals(notification.totalCentrals);
     });
 
     return () => {
